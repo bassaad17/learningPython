da83dc603fae6df9313049a9547ae28b0cf71019
AssaadB <bilal.assaad@stvinc.com>
2021-01-06 12:16:57 -0500

    Updated print statement from stv local laptop

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,2 @@
 print("Hello World!!!")
-<<<<<<< HEAD
 print("Hello Wolrd!!! - From STV Local Laptop")
-=======
-print("Hello World!!! - From STV IPCSv5")
-print("more here")
->>>>>>> be5ec85c695de20e5f3cc984b45c53a9b6394240
